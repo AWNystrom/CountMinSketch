fd10503339ad877fd3b1d9c28275ef6afe393517
Andrew Nystrom <awnystrom@gmail.com>
2016-09-06 21:16:35 -0700

    v uptick

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="count_min_sketch",
-    version="3.0.0",
+    version="3.0.1",
     packages=['count_min_sketch/'],
     description="A fast and accurate Count-Min Sketch data structure.",
     author="Andrew Nystrom",
@@ -13,7 +13,7 @@ setup(
     url="https://github.com/AWNystrom/CountMinSketch",
     keywords=["count", "min", "sketch", "fast", "pure"],
     license="Apache 2.0",
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     classifiers=["Programming Language :: Python",
                  "Programming Language :: Python :: 2.7",
                  "Programming Language :: Python :: 2",
